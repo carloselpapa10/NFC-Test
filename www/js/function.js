48232b6d1704f038c1e5d8d6bda905aef80f59f6
carloselpapa10 <c.avendano10@gmail.com>
2016-12-18 07:43:25 -0500

    Update function.js

--- www/js/function.js
+++ www/js/function.js
@@ -3,34 +3,24 @@ var app = angular.module("app", ['onsen','ngCordova.plugins.nfc']);
 app.controller("AppController", function($scope,$http,$cordovaNfc, $cordovaNfcUtil){
 	
 	$cordovaNfc.then(function(nfcInstance){
-
-        //Use the plugins interface as you go, in a more "angular" way
-      nfcInstance.addNdefListener(function(event){
-            //Callback when ndef got triggered
+      nfcInstance.addNdefListener(function(nfcEvent){
 			alert("123");
       })
       .then(
-        //Success callback
         function(event){
-			alert("bound success");
             console.log("bound success");
         },
-        //Fail callback
         function(err){
-			alert("error 1");
             console.log("error");
         });
 		
-		nfcInstance.addTagDiscoveredListener(function(event){
-			alert("345");
+		nfcInstance.addTagDiscoveredListener(function(nfcEvent){
+			alert(nfcEvent.tag.id);
 		})
 		.then(
-        //Success callback
         function(event){
-			alert("bound success 2");
             console.log("bound success");
         },
-        //Fail callback
         function(err){
 			alert("error 2");
             console.log("error");
@@ -38,7 +28,6 @@ app.controller("AppController", function($scope,$http,$cordovaNfc, $cordovaNfcUt
    });
 
    $cordovaNfcUtil.then(function(nfcUtil){
-		alert("nfcUtil");
         console.log( nfcUtil.bytesToString("some bytes") )
    }); 
 	   
