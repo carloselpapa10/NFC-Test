67d48cab4dbc1000563e694a527ade89ef8937f9
carloselpapa10 <c.avendano10@gmail.com>
2016-12-18 08:03:58 -0500

    Update function.js

--- www/js/function.js
+++ www/js/function.js
@@ -1,13 +1,44 @@
 var app = angular.module("app", ['onsen','ngCordova.plugins.nfc']);
 
 app.controller("AppController", function($scope,$http,$cordovaNfc, $cordovaNfcUtil){
+
+	$scope.user = {
+        ID: '',
+        NAME: '',
+		LASTNAME: '',
+        PHOTO: '',
+        TYPE: '',
+        PHONE: '',
+        EMAIL: ''
+    };
 	
-	$scope.failure = function(reason){
-		navigator.notification.alert(reason, function() {}, "There was a problem");
-	}
+	$scope.onSearchUser = function(ID){
 	
-	$scope.addTagD = function(nfcEvent){
-		alert(nfcEvent.tag.id);
+		/*go to search student information*/
+		$scope.user = [{ID: '4,-13,109,-6,-39,63,-128',
+				NAME: 'Carlos',
+				LASTNAME: 'Avendano',
+				PHOTO: 'path',
+				TYPE: 'Student',
+				PHONE: '3003940576',
+				EMAIL: 'c.avendano10@gmail.com'},
+				{ID: '4,-98,-73,-118,-38,63,-128',
+				NAME: 'Kelwin',
+				LASTNAME: 'Payares',
+				PHOTO: 'path',
+				TYPE: 'Professor',
+				PHONE: '3288046004',
+				EMAIL: 'stevin_2209@hotmail.com'}];
+				
+		if(ID == $scope.user[0].ID){
+			alert($scope.user[0].NAME);
+			document.getElementById("userData").textContent=$scope.user[0].NAME+" ("+$scope.user[0].TYPE+")";
+		}else if(ID == $scope.user[1].ID){
+			alert($scope.user[0].NAME);
+			document.getElementById("userData").textContent=$scope.user[1].NAME+" ("+$scope.user[1].TYPE+")";
+		}else{
+			document.getElementById("userData").textContent="Unknown User";
+		}
 	}
 	
 	$cordovaNfc.then(function(nfcInstance){
@@ -18,16 +49,23 @@ app.controller("AppController", function($scope,$http,$cordovaNfc, $cordovaNfcUt
         function(event){
             console.log("bound success");
         },
-        $scope.failure);
+        function(err){
+			alert("error addNdefListener");
+            console.log("error");
+        });
 		
-		nfcInstance.addTagDiscoveredListener(
-		$scope.addTagD
+		nfcInstance.addTagDiscoveredListener(function(nfcEvent){
+			navigator.notification.vibrate(200); 
+			$scope.onSearchUser(nfcEvent.tag.id);
+		})
 		.then(
         function(event){
-		alert("good");
             console.log("bound success");
         },
-        $scope.failure);
+        function(err){
+			alert("error addTagDiscoveredListener");
+            console.log("error");
+        });
    });
 
    $cordovaNfcUtil.then(function(nfcUtil){
